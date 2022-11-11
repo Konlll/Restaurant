327e9e5feb9d75c65ec8f452ef234720f1b66720
スケッチ <54027485+suketti@users.noreply.github.com>
2022-11-11 22:31:17 +0100

    Implement SQL connection

--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
+
+var db *sql.DB
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello world :)"))
 }
 
 func main() {
+	db, err := sql.Open("mysql", "dbuser:qweasd@tcp(localhost:3306)/restaurant")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println("MySQL connection succesful")
+	defer db.Close()
 	mux := mux.NewRouter()
 
 	mux.HandleFunc("/", Handler).Methods("GET")
